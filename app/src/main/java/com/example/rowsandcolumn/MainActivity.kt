119ee5c8df4d4628e7aba96876e14b30b03029d7
nidhithakur <thakurnidhi54048@gmail.com>
2024-11-03 20:02:20 +0530

    Modifiers Tutorial

--- app/src/main/java/com/example/rowsandcolumn/MainActivity.kt
+++ app/src/main/java/com/example/rowsandcolumn/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.rowsandcolumn.ui.theme.RowsAndColumnTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,19 +38,32 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    Greeting("Android")
                     Column(
-                        modifier = Modifier.background(Color.Green).fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceAround
+                        modifier = Modifier
+                            .background(Color.Green)
+                            .fillMaxSize()
+                            .width(600.dp)
+//                            .requiredWidth(600.dp)
+                            .border(5.dp, Color.Magenta)
+                            .padding(16.dp)
+                            .border(5.dp, Color.Blue)
+                            .padding(16.dp)
+                            .border(5.dp, Color.Red)
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.SpaceAround
                     ) {
-                        Text(text = "Hi")
+                        Text(
+                            text = "Hi",
+//                            modifier = Modifier.offset(50.dp, 20.dp)
+                        )
+                        Spacer(modifier = Modifier.height(50.dp))
                         Text(text = "World")
                         Text(text = "Bye")
                     }
-                    Row (
+                    Row(
                         modifier = Modifier.fillMaxSize(),
                         horizontalArrangement = Arrangement.Center,
                         verticalAlignment = Alignment.Bottom
-                    ){
+                    ) {
                         Text(text = "Hi")
                         Text(text = "World")
                     }
