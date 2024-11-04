1d4ce80257b785815e6b37cf004d8b00eb444c50
nidhithakur <thakurnidhi54048@gmail.com>
2024-11-04 14:21:16 +0530

    ImageCard Tutorial

--- app/src/main/java/com/example/rowsandcolumn/MainActivity.kt
+++ app/src/main/java/com/example/rowsandcolumn/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -22,6 +24,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rowsandcolumn.ui.theme.RowsAndColumnTheme
@@ -67,6 +71,14 @@ class MainActivity : ComponentActivity() {
                         Text(text = "Hi")
                         Text(text = "World")
                     }
+                    Box (modifier = Modifier.fillMaxWidth(0.5f)){
+                        ImageCard.ImageCardMethod(
+                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                            contentDescription = "Playing",
+                            title = "Playing in the snow"
+                        )
+                    }
+
                 }
             }
         }
